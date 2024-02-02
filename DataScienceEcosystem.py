6cf8b5d736fe57890891fe8e08647ee086ebca7e
Selma Kinacioglu <132160283+Selmabanzema@users.noreply.github.com>
2024-02-03 00:30:31 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+
+# In[1]:
+
+
+list=["R","Python","SQL","Julia","C++","SPSS"]
+print(list)
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# In[4]:
+
+
+list=["Pandas","Numpy","Matplotlib","Seaborn"]
+print(list)
+
+
+# Data Science Tools
+
+# In[6]:
+
+
+Tools=["Jypter Notebook","Pycharm","Pycharm","MLFlow"]
+print(Tools)
+
+
+# Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[7]:
+
+
+list=["+","*","/","-"]
+print("list")
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[8]:
+
+
+(2*3)/2+(5*3-1)
+
+
+# his will convert 200 minutes to hours by diving by 60
+
+# In[9]:
+
+
+min= 200
+hour= 200/60 
+print(hour)
+
+
+# # Objectives
+
+# In[10]:
+
+
+list=["R","SQL","Seaborn","Python","Weka"]
+print(list)
+
+
+# ## Selma Kınacıoğlu
+
+# In[ ]:
+
+
+
+
